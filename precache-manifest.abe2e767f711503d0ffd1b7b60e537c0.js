89a99e10d0c6a9426ce086f8436c8370b2c0d542
Mk13Ghoul <mayankkumarj5@gmail.com>
2022-02-28 16:36:04 +0530

    Updates

---
+++ precache-manifest.abe2e767f711503d0ffd1b7b60e537c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0b6ed6c423ede4685ee7ff2cd9fab0",
+    "url": "/JavaScript-Calculator/index.html"
+  },
+  {
+    "revision": "d8b8a78d79918b858dd5",
+    "url": "/JavaScript-Calculator/static/css/main.bcff967d.chunk.css"
+  },
+  {
+    "revision": "9217de5d703d4a578998",
+    "url": "/JavaScript-Calculator/static/js/2.602f85dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JavaScript-Calculator/static/js/2.602f85dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b8a78d79918b858dd5",
+    "url": "/JavaScript-Calculator/static/js/main.fa893f68.chunk.js"
+  },
+  {
+    "revision": "f80b0b28f00903c41889",
+    "url": "/JavaScript-Calculator/static/js/runtime-main.e6b69808.js"
+  }
+]);
